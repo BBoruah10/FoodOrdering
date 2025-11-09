590f94002f16c414c3600a5d6b0bcdce1347339c
bhaskar <baruahbhaskar10@gmail.com>
2025-11-09 09:39:37 +0530

    added a Hello COntroller for checking

---
+++ src/main/java/com/Food/FoodOrdering/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.Food.FoodOrdering.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(customizer->customizer.disable());
+        http.authorizeHttpRequests(requests->requests
+                .requestMatchers("/hello")
+                .permitAll()
+                .anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
